d0f7ed06345bcd010e3731d82d505ea78c76246e
Imran Bin Gifary (System Delta or Imran_Delta Online) <imran.sdelta@gmail.com>
2025-09-04 00:17:54 +0600

    Add README.md and update setup.py for pip install

--- setup.py
+++ setup.py
@@ -3,20 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='gisl-data-library',
     version='0.0.1',
-    author='Imran Bin Gifary (System Delta or Imran_Delta Online)',
-    author_email='imran.sdelta@gmail.com',
-    description='A simple Python library for retrieving Genshin Impact character and material data.',
-    long_description='A simple Python library for retrieving Genshin Impact character and material data from a JSON file. This is a work in progress.',
-    long_description_content_type='text/plain',
-    url='https://github.com/Imran-Delta/GI-Static-Data-Library',
     packages=find_packages(),
-    package_data={
-        'gisl_data_library': ['gisl_data.json'],
-    },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    install_requires=[
+        requests>=2.25.1
     ],
-    python_requires='>=3.7',
+    author='Imran Bin Gifary (System Delta or Imran Delta Online)',
+    author_email='imran.sdelta@gmail.com',
+    description='A simple Python library for retrieving Genshin Impact character and material data from a JSON file. This is a work in progress.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Imran-Delta/GI-Static-Data-Library',
 )
