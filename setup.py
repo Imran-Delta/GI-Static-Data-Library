a323f129d424a139f0e7f95cda4d743f131ea133
Imran <gifarybinimran@gmail.com>
2025-11-03 19:24:37 +0600

    Update setup.py
    PKG Rename and new dependency.

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gisl-data-library',
-    version='0.1.5',
+    name='genshin-impact',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'gisl_data_library': ['gisl_data.json'],
+        'genshin_impact': ['gisl_data.json'],
     },
-    install_requires=[],
+    install_requires=[
+      'packaging',
+      ],
     author='Imran Bin Gifary (System Delta or Imran Delta Online)',
     author_email='imran.sdelta@gmail.com',
-    description='A static library of Genshin item/char info. (WIP) - Albedo added.',
+    description='A static library of Genshin item/char info. (WIP)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Imran-Delta/GI-Static-Data-Library',
